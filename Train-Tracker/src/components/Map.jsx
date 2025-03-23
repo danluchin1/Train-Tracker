929b4b2de33ba5715458d5d89f39974f2b4c4d41
Luchin Dan <144890437+danluchin1@users.noreply.github.com>
2025-03-23 16:13:05 +0200

    Updated Map.jsx, index.css and added Map.css

--- Train-Tracker/src/components/Map.jsx
+++ Train-Tracker/src/components/Map.jsx
@@ -1,13 +1,39 @@
-import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { getCurrentTrainLocations } from "../services/api";
+import "../css/Map.css";
 import { useEffect, useState } from "react";
-import { divIcon } from "leaflet";
 
 function Map() {
     const [trains, setTrains] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
+    
+    let greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+      let goldIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+      let redIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
 
     useEffect(() => {
         const loadCurrentTrainLocations = async () => {
@@ -23,32 +49,46 @@ function Map() {
         };
 
         loadCurrentTrainLocations();
+        const interval = setInterval(loadCurrentTrainLocations, 30000);
+
+        return () => clearInterval(interval);
     }, []);
 
     return (
         <>
             {error && <div>{error}</div>}
 
-            <MapContainer center={[60.192059, 24.945831]} zoom={11} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {loading ? (<div className="loading">Loading...</div>) : (
-                    trains.map((train) => {
-                        const [longitude, latitude] = train.location.coordinates;
-                        const position = [latitude, longitude];
-                        return (
-                            <Marker key={train.trainNumber} position={position}>
-                                <Popup>
-                                    Train number: {train.trainNumber} <br/>
-                                    Speed: {train.speed} km/h
-                                </Popup>
-                            </Marker>
-                        );
-                    })
-                )}
-            </MapContainer>
+            <div className="wrap-container">
+                <MapContainer center={[60.192059, 24.945831]} zoom={11} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {loading ? (<div className="loading-spinner"></div>) : (
+                        trains.map((train) => {
+                            const [longitude, latitude] = train.location.coordinates;
+                            const position = [latitude, longitude];
+
+                            let icon = redIcon;
+                            if(train.speed >= 50 && train.speed <= 100){
+                                icon = goldIcon;
+                            }else if(train.speed > 100){
+                                icon = greenIcon;
+                            }
+
+                            return (
+                                <Marker key={train.trainNumber} position={position} icon={icon}>
+                                    <Popup>
+                                        Train number: {train.trainNumber} <br />
+                                        Departure Date: {train.departureDate} <br />
+                                        Speed: {train.speed} km/h
+                                    </Popup>
+                                </Marker>
+                            );
+                        })
+                    )}
+                </MapContainer>
+            </div>
         </>
     );
 }

5fa37e9dcd7e504a17a03aa7902508e40426f498
Luchin Dan <144890437+danluchin1@users.noreply.github.com>
2025-05-16 13:32:48 +0300

    Refactor API endpoint URLs to use a base URL for production and development environments

--- Train-Tracker/src/services/api.js
+++ Train-Tracker/src/services/api.js
@@ -1,8 +1,11 @@
 const bbox = "20.170898,59.512029,31.904297,70.214875";
 
+const baseUrl = import.meta.env.PROD
+  ? 'https://rata.digitraffic.fi/api/v1'
+  : '/api';
 
 export const getCurrentTrainLocations = async () => {
-    const response = await fetch(`/api/train-locations/latest?bbox=${bbox}`);
+    const response = await fetch(`${baseUrl}/train-locations/latest?bbox=${bbox}`);
     const data = await response.json();
     
     // Extract Last Updated Timestamp from headers
@@ -13,7 +16,7 @@ export const getCurrentTrainLocations = async () => {
 
 export const getTrainDetails = async (trainNumber) => {
     const today = new Date().toISOString().split("T")[0];
-    const response = await fetch(`/api/trains/${today}/${trainNumber}`);
+    const response = await fetch(`${baseUrl}/trains/${today}/${trainNumber}`);
     const data = await response.json();
     return data;
 }
